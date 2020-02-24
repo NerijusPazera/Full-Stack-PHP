7a869ffa7c0bbf67f32a275640cdebe3ee74cc2e
NerijusPazera <nerijus.pazera@gmail.com>
2020-02-24 12:38:50 +0200

    Uzduotis #2

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     $title = 'Lenta | PHP Fight Club';
     $p = date('Y-m-d H:i:s') . ' Nerijus grįžo į PHPFIGHTCLUB`ą !';
+    $p_2 = date('F', strtotime('-1 month')) . ' Nerijus priėmė teisingą sprendimą.';
 ?>
 
 
@@ -10,9 +11,10 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title><?php print$title ?></title>
+        <title><?php print $title ?></title>
     </head>
     <body>
         <p><?php print $p?></p>
+        <p><?php print $p_2 ?></p>
     </body>
 </html>
