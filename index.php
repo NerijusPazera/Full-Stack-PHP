e3e6b73dea51a8fc8b3cbffdb0736ee7c97c64f0
NerijusPazera <nerijus.pazera@gmail.com>
2020-02-24 21:54:10 +0200

    Uzduotis #5

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-    $title = 'Facts | PHP Fight Club';
-    $s = rand(2,3);
-    $p = 'Per šią sekundę organizmas pagamino ' . $s . ' mil. kraujo ląstelių.';
+$title = 'Coin- Flip';
+$x = rand(1, 2);
+$class = 0;
+$span = 0;
+
+if ($x == 1) {
+    $class = 'heads';
+} else {
+    $class = 'tails';
+}
+$span = $class;
 ?>
 
 
@@ -12,8 +20,33 @@
     <head>
         <meta charset="utf-8">
         <title><?php print $title ?></title>
+        <style>
+            body {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+            span {
+                text-transform: capitalize;
+                font-size: 50px;
+                font-weight: bold;
+            }
+            div {
+                height: 500px;
+                width: 500px;
+                background-size: cover;
+            }
+            .heads {
+                background-image: url("https://images-na.ssl-images-amazon.com/images/I/61hsuS11FPL._SY355_.jpg");
+            }
+            .tails {
+                background-image: url("https://image.dhgate.com/0x0p/f2/albu/g6/M00/93/6F/rBVaR1tYhY-ATc8QAAJM0w8EFu8639.jpg");
+            }
+        </style>
     </head>
     <body>
-        <p><?php print $p?></p>
+        <div class="<?php print $class ?>"></div>
+        <span><?php print $span ?></span>
     </body>
 </html>
