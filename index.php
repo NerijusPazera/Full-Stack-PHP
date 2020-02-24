e6dc4b967c99712024b2022ad7495e10c6b47331
NerijusPazera <nerijus.pazera@gmail.com>
2020-02-24 12:23:53 +0200

    Uzduotis #1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $title = 'Lenta | PHP Fight Club';
+    $p = date('Y-m-d H:i:s') . ' Nerijus grįžo į PHPFIGHTCLUB`ą !';
+?>
+
+
+
+
+
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title><?php print$title ?></title>
+    </head>
+    <body>
+        <p><?php print $p?></p>
+    </body>
+</html>
