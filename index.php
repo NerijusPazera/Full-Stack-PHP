6c385329f1da453b04985a2650ae574cdd64ad2f
NerijusPazera <nerijus.pazera@gmail.com>
2020-03-17 12:23:36 +0200

    Funkcijos #2

--- index.php
+++ index.php
@@ -2,32 +2,34 @@
 
 $title = 'Funkcijos';
 
-$a = rand(1, 10);
-$b = rand(10, 20);
+$x = rand(2, 10);
 
-/**
- * @param $x
- * @param $y
- * @return mixed
- */
-function suma($x, $y) {
-    return $x + $y;
+function pirminis($x) {
+    for ($y = 2; $y <= $x / 2; $y++) {
+        if($x % $y == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-$suma = suma($a, $b);
-
-$h1 = "$a ir $b suma: $suma";
+if(pirminis($x)) {
+    $h1 = "Skaicius $x yra pirminis";
+}
+else {
+    $h1 = "Skaicius $x yra nepirminis";
+}
 
 ?>
 
 
 <html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <title><?php print $title; ?></title>
-</head>
-<body>
-    <h1><?php print $h1; ?></h1>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <title><?php print $title; ?></title>
+    </head>
+    <body>
+        <h1><?php print $h1; ?></h1>
+    </body>
 </html>
